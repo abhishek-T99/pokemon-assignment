3a9a82ead3c3736e3167cbc6b587f9ee91137f0a
abhishek-T99 <timilsina.abhishek55@gmail.com>
2024-07-24 04:50:46 +0545

    feat(api): Changed logic to fetch all the data dynamically without using static pagination

--- app/utils/fetch_pokemon.py
+++ app/utils/fetch_pokemon.py
@@ -1,4 +1,4 @@
-import aiohttp
+import httpx
 import asyncio
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -6,37 +6,37 @@ from app.models.pokemon import Pokemon
 
 
 async def fetch_pokemon_details(url: str):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            response.raise_for_status()
-            data = await response.json()
-
-    async def fetch_details(pokemon_url: str):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(pokemon_url) as response:
-                response.raise_for_status()
-                return await response.json()
-
-    # Create a list of tasks to fetch details for each Pokémon
-    tasks = [fetch_details(result['url']) for result in data['results']]
-    details = await asyncio.gather(*tasks)
-    
-    # Process and structure the Pokémon data
     pokemon_data = []
-    for i, detail in enumerate(details):
-        pokemon = {
-            "name": data['results'][i]['name'],
-            "image": detail["sprites"]["other"]["official-artwork"]["front_default"],
-            "types": [t["type"]["name"] for t in detail["types"]]
-        }
-        pokemon_data.append(pokemon)
-    
+    async with httpx.AsyncClient() as client:
+        while url:
+            response = await client.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            tasks = [fetch_pokemon_detail(client, result['url']) for result in data['results']]
+            pokemon_details = await asyncio.gather(*tasks)
+
+            for details in pokemon_details:
+                pokemon_data.append({
+                    "name": details["name"],
+                    "image": details["sprites"]["other"]["official-artwork"]["front_default"],
+                    "types": [t["type"]["name"] for t in details["types"]],
+                })
+
+            url = data.get("next")
+
     return pokemon_data
+
+
+async def fetch_pokemon_detail(client: httpx.AsyncClient, url: str):
+    response = await client.get(url)
+    response.raise_for_status()
+    return response.json()
 
 
 async def store_pokemon_db(db_session: AsyncSession, pokemon_list: list):
     for pokemon in pokemon_list:
-        # Check if Pokémon with the same name already exists
+        # Check if Pokemon with the same name already exists
         result = await db_session.execute(select(Pokemon).where(Pokemon.name == pokemon['name']))
         existing_pokemon = result.scalars().first()
 
